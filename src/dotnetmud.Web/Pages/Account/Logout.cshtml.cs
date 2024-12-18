4a19e13fd4da1f4ed5e597f6c818f92fe2cc77c2
Adrian Hall <adhal@microsoft.com>
2024-12-18 10:24:14 -0800

    Added sign out capabilities.

---
+++ src/dotnetmud.Web/Pages/Account/Logout.cshtml.cs
@@ -0,0 +1,17 @@
+using dotnetmud.Web.Database.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dotnetmud.Web.Pages.Account;
+
+public class LogoutModel(SignInManager<ApplicationUser> signInManager, ILogger<LogoutModel> logger) : PageModel
+{
+    public async Task<IActionResult> OnPost(string? returnUrl = null)
+    {
+        string username = User.Identity?.Name ?? "-";
+        await signInManager.SignOutAsync();
+        logger.LogInformation("User {name} logged out.", username);
+        return returnUrl != null ? LocalRedirect(returnUrl) : RedirectToPage();
+    }
+}
